c5966f0ef50b9dcd0b7dfe777483441338660ff9
kaneza1234 <167434740+kaneza1234@users.noreply.github.com>
2024-04-24 09:43:43 +0200

    Add files via upload

---
+++ nb.js
@@ -0,0 +1,32 @@
+// let number1 = 12;
+// if (number1 <= 19) {
+//     console.log("your young");
+// }
+// else {
+//     console.log("your old");
+// }
+// let num1 = 12;
+// let num2 = 88;
+// if (num1 + num2 == 100) {
+//     console.log("is low");
+// }
+// else {
+//     console.log("is greater");
+// }
+let grade = 1,50;
+if (grade < 50) {
+    console.log("Fail");
+}
+else if (grade <= 60) {
+    console.log("grade C")
+}
+else if (grade <= 70) {
+    console.log("grade B")
+}
+
+else if (grade <= 100) {
+    console.log("grade A")
+}
+else {
+    console.log("Please Enter Valid Number")
+}
